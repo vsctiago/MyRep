3d06c824e545cf389ed3e62742961855c492ae4e
vsctiago <vsctiago@yahoo.com>
2015-12-14 20:47:52 +0000

    adding small calc

--- sample.c
+++ sample.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main(){
-printf("hello world");
-printf("lolollollol");
+printf("hello world\n");
+printf("lolollollol\n");
+int n1 = 10;
+int n2 = 20;
+int sum;
+printf("%d\n",sum = n1 + n2;
+
 return 0;
 }
